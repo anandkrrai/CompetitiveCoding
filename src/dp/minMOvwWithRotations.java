b1fce17f64da242896e18e15f9f98deee0e2df23
anand rai <kumar.anand.rai@gmail.com>
2019-11-01 01:12:03 +0530

    leetcode

--- src/dp/minMOvwWithRotations.java
+++ src/dp/minMOvwWithRotations.java
@@ -6,14 +6,16 @@ public class minMOvwWithRotations {
 
 	public static class Pair {
 		int row, col, steps, pos;
+		String psf;
 
 		// 0->horizontal
 		// 1-vertical
-		public Pair(int row, int col, int steps, int pos) {
+		public Pair(int row, int col, int steps, int pos, String psf) {
 			this.row = row;
 			this.col = col;
 			this.steps = steps;
 			this.pos = pos;
+			this.psf = psf;
 		}
 	}
 
@@ -25,13 +27,55 @@ public class minMOvwWithRotations {
 		else
 			return true;
 	}
+//
+//	public static int minimumMoves(int[][] grid) {
+//		LinkedList<Pair> q = new LinkedList<>();
+//
+//		Pair p = new Pair(0, 1, 0, 0);
+//		q.add(p);
+//		HashSet<String> visited = new HashSet<>();
+//
+//		while (q.size() != 0) {
+//			Pair rem = q.removeFirst();
+//
+//			String key = rem.row + "*" + rem.col + "*" + rem.pos;
+//			System.out.println(key);
+//
+//			if (rem.row == grid.length - 1 && rem.col == grid[0].length - 1)
+//				return rem.steps;
+//			else if (visited.contains(key))
+//				continue;
+//			visited.add(key);
+//			if (rem.pos == 0) {
+//
+//				if (isValid(grid, rem.row, rem.col + 1))
+//					q.addLast(new Pair(rem.row, rem.col + 1, rem.steps + 1, 0));
+//
+////				if (isValid(grid, rem.row + 1, rem.col) && isValid(grid, rem.row + 1, rem.col - 1))
+////					q.addLast(new Pair(rem.row + 1, rem.col - 1, rem.steps + 1, 1));
+//
+//				if (isValid(grid, rem.row + 1, rem.col - 1))
+//					q.addLast(new Pair(rem.row + 1, rem.col - 1, rem.steps + 1, 1));
+//			}
+//			if (rem.pos == 1) {
+//
+//				if (isValid(grid, rem.row + 1, rem.col))
+//					q.addLast(new Pair(rem.row + 1, rem.col, rem.steps + 1, 1));
+//
+//				if (isValid(grid, rem.row, rem.col + 1) && isValid(grid, rem.row - 1, rem.col + 1))
+//					q.addLast(new Pair(rem.row - 1, rem.col + 1, rem.steps + 1, 0));
+//			}
+//		}
+//		return -1;
+//	}
 
-	public static int minimumMoves(int[][] grid) {
+	public static int minimumMovesNew(int[][] grid) {
 		LinkedList<Pair> q = new LinkedList<>();
 
-		Pair p = new Pair(0, 1, 0, 0);
+		Pair p = new Pair(0, 1, 0, 0, "");
 		q.add(p);
 		HashSet<String> visited = new HashSet<>();
+		String down = "down,", right = "right,", rotatea = " rotate counterclockwise,", rotatec = "rotate clockwise,";
 
 		while (q.size() != 0) {
 			Pair rem = q.removeFirst();
@@ -39,26 +83,35 @@ public class minMOvwWithRotations {
 			String key = rem.row + "*" + rem.col + "*" + rem.pos;
 			System.out.println(key);
 
-			if (rem.row == grid.length - 1 && rem.col == grid[0].length - 1)
+			if (rem.row == grid.length - 1 && rem.col == grid[0].length - 1 && isValid(grid, rem.row,rem.col-1) && rem.pos ==0) {
+				System.out.println(rem.psf);
 				return rem.steps;
+			}
+
 			else if (visited.contains(key))
 				continue;
 			visited.add(key);
 			if (rem.pos == 0) {
 
 				if (isValid(grid, rem.row, rem.col + 1))
-					q.addLast(new Pair(rem.row, rem.col + 1, rem.steps + 1, 0));
+					q.addLast(new Pair(rem.row, rem.col + 1, rem.steps + 1, 0, rem.psf + right));
 
 				if (isValid(grid, rem.row + 1, rem.col) && isValid(grid, rem.row + 1, rem.col - 1))
-					q.addLast(new Pair(rem.row + 1, rem.col - 1, rem.steps + 1, 1));
+					q.addLast(new Pair(rem.row + 1, rem.col - 1, rem.steps + 1, 1, rem.psf + rotatec));
+
+				if (isValid(grid, rem.row + 1, rem.col) && isValid(grid, rem.row + 1, rem.col - 1))
+					q.addLast(new Pair(rem.row + 1, rem.col, rem.steps + 1, 0, rem.psf + down));
 			}
 			if (rem.pos == 1) {
 
 				if (isValid(grid, rem.row + 1, rem.col))
-					q.addLast(new Pair(rem.row + 1, rem.col, rem.steps + 1, 1));
+					q.addLast(new Pair(rem.row + 1, rem.col, rem.steps + 1, 1, rem.psf + down));
 
 				if (isValid(grid, rem.row, rem.col + 1) && isValid(grid, rem.row - 1, rem.col + 1))
-					q.addLast(new Pair(rem.row - 1, rem.col + 1, rem.steps + 1, 0));
+					q.addLast(new Pair(rem.row - 1, rem.col + 1, rem.steps + 1, 0, rem.psf + rotatea));
+
+				if (isValid(grid, rem.row, rem.col + 1) && isValid(grid, rem.row - 1, rem.col + 1))
+					q.addLast(new Pair(rem.row, rem.col + 1, rem.steps + 1, 1, rem.psf + right));
 			}
 		}
 		return -1;
@@ -66,14 +119,17 @@ public class minMOvwWithRotations {
 
 	public static void main(String[] args) {
 
-		int[][] grid = { { 0, 0, 0, 0, 0, 1 }, { 1, 1, 0, 0, 1, 0 }, { 0, 0, 0, 0, 1, 1 }, { 0, 0, 1, 0, 1, 0 },
+		int[][] grid1 = { { 0, 0, 0, 0, 0, 1 }, { 1, 1, 0, 0, 1, 0 }, { 0, 0, 0, 0, 1, 1 }, { 0, 0, 1, 0, 1, 0 },
 				{ 0, 1, 1, 0, 0, 0 }, { 0, 1, 1, 0, 0, 0 } };
-		for(int[] arr : grid) {
-			for(int val : arr)
-				System.out.print(val+" ");
+
+		int[][] grid = { { 0, 0, 1, 1, 1, 1 }, { 0, 0, 0, 0, 1, 1 }, { 1, 1, 0, 0, 0, 1 }, { 1, 1, 1, 0, 0, 1 },
+				{ 1, 1, 1, 0, 0, 1 }, { 1, 1, 1, 0, 0, 0 } };
+		for (int[] arr : grid) {
+			for (int val : arr)
+				System.out.print(val + " ");
 			System.out.println();
 		}
-		System.out.println(minimumMoves(grid));
+		System.out.println(minimumMovesNew(grid));
 	}
 
 }
