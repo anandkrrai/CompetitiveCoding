5927f20b29ffa87f8768f339e6f1cb467c26e9c2
anand rai <kumar.anand.rai@gmail.com>
2020-01-12 10:54:00 +0530

    leetcode

--- src/arraysAndstrings/concatenationWithItself.java
+++ src/arraysAndstrings/concatenationWithItself.java
@@ -1,6 +1,22 @@
 package arraysAndstrings;
 
+import java.util.HashSet;
+
 public class concatenationWithItself {
+	public int distinctEchoSubstrings(String text) {
+		HashSet<String> set = new HashSet<String>();
+		for (int i = 0; i < text.length(); ++i) {
+			calc(set, text.substring(i));
+		}
+		return set.size();
+	}
+
+	private void calc(HashSet<String> set, String text) {
+		for (int i = 0; i < text.length() / 2; ++i) {
+			if (text.substring(0, i + 1).equals(text.substring(i + 1, 2 * (i + 1))))
+				set.add(text.substring(0, i + 1));
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/arraysAndstrings/matrixKSum.java
+++ src/arraysAndstrings/matrixKSum.java
@@ -2,4 +2,18 @@ package arraysAndstrings;
 
 public class matrixKSum {
 
+	public int[][] matrixBlockSum(int[][] mat, int K) {
+		int[][] arr = new int[mat.length][mat[0].length];
+
+		for (int i = 0; i < arr.length; ++i) {
+			for (int j = 0; j < arr[0].length; ++j) {
+				for (int i1 = Math.max(0, i - K); i1 <= Math.min(i + K, mat.length); ++i1) {
+					for (int j1 = Math.max(0, j - K); j1 <= Math.min(j + K, mat.length); ++j1) {
+						arr[i][j] += mat[i][j];
+					}
+				}
+			}
+		}
+		return arr;
+	}
 }
